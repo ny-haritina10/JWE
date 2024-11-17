9b94018a760812363475d072b9d2844fa31d477e
ny-haritina10 <142341722+ny-haritina10@users.noreply.github.com>
2024-11-17 18:17:51 +0100

    feat: orm crud

---
+++ ORM/src/mg/jwe/orm/annotations/ForeignKey.java
@@ -0,0 +1,14 @@
+package mg.jwe.orm.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface ForeignKey {
+    String table();       
+    String column();     
+    boolean lazy() default true;  
+}
